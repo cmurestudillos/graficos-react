aae72f0216bb468828a4c95876d3b736b1e11a7f
Carlos Mur <cmurdeveloper@gmail.com>
2025-08-25 09:30:19 +0200

    feat: Add new rules & config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,27 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  server: {
+    port: 3000,
+  },
   build: {
-    outDir: './build'
-  }
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+        },
+      },
+    },
+  },
+  preview: {
+    port: 3000,
+  },
 })
